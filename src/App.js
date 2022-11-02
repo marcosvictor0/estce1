ff1987df09de0eea23dd4c2710892c6e7e09c084
Marcos Victor <marcos_victor159@hotmail.com>
2022-11-02 19:33:43 -0300

    atualizações css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import "./App.css";
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p className="nome-princ"
-        >EsTce</p>
-        <div className="div-bttn">
-          <input
+        <p className="nome-princ">EsTce</p>
+        <input
             className="bttn-pesquisa"
             id="input"
             type="search"
             placeholder="Pesquise"
             aria-live="polite"
           ></input>
-        </div>
       </header>
+      <footer>
+        <div>teste</div>
+      </footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
