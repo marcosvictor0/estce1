8874ae6d8f96392fd14c78615be8a49059cd2fc1
Marcos Victor <marcos_victor159@hotmail.com>
2022-11-02 20:58:19 -0300

    feito atualizações html e css

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <main className="App-header">
         <p className="nome-princ">EsTce</p>
         <input
-            className="bttn-pesquisa"
-            id="input"
-            type="search"
-            placeholder="Pesquise"
-            aria-live="polite"
-          ></input>
-      </header>
+          className="bttn-pesquisa"
+          id="input"
+          type="search"
+          placeholder="Pesquisar"
+          aria-live="polite"
+        ></input>
+      </main>
       <footer>
-        <div>teste</div>
+        <div className="footer-fim">
+          Tribunal de Contas do Piauí 
+          <br></br>
+          Av. Pedro Freitas, 2100 - Centro
+          Administrativo - CEP: 64018-900 - Teresina-PI 
+          <br></br>
+          <br></br>
+          DTIF - Divisão de
+          Desenvolvimento de Softwares© - 2022{' '}
+        </div>
       </footer>
     </div>
   )
