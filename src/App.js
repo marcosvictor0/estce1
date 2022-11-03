f904282473a1200b34e4d765de165bcfb1ff3e02
Marcos <marcos_victor159@hotmail.com>
2022-11-03 09:46:30 -0300

    ajustes no front

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
-import './App.css'
+import "./App.css";
 
 function App() {
+  
   return (
     <div className="App">
       <main className="App-header">
         <p className="nome-princ">EsTce</p>
         <input
-          className="bttn-pesquisa"
+          className="campo-pesquisa"
           id="input"
           type="search"
           placeholder="Pesquisar"
           aria-live="polite"
         ></input>
+        <button className="btn-pesquisa">
+          <a href="index.html">Pesquisar no EsTce</a>
+        </button>
       </main>
       <footer>
         <div className="footer-fim">
-          Tribunal de Contas do Piauí 
+          Tribunal de Contas do Piauí
           <br></br>
-          Av. Pedro Freitas, 2100 - Centro
-          Administrativo - CEP: 64018-900 - Teresina-PI 
+          Av. Pedro Freitas, 2100 - Centro Administrativo - CEP: 64018-900 -
+          Teresina-PI
           <br></br>
           <br></br>
-          DTIF - Divisão de
-          Desenvolvimento de Softwares© - 2022{' '}
+          DTIF - Divisão de Desenvolvimento de Softwares© - 2022{" "}
         </div>
       </footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
